f6b7e7f3b9a336cea8a0dc87be75d2d47353df47
김서준 <jskim332@naver.com>
2017-01-19 16:54:37 +0900

    init

---
+++ RockScissorPaper/src/com/clueless/rock_scissor_paper/RockScissorPaper.java
@@ -0,0 +1,57 @@
+package com.clueless.rock_scissor_paper;
+
+import java.util.Scanner;
+
+public class RockScissorPaper {
+	public static void main(String[] args) {
+		
+		String me;
+		int num;
+		String computer;
+		int com;
+		String re;
+		int mecount=0;
+		int comcount=0;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		while(true){
+			System.out.println("");
+			while(true){
+				System.out.println("가위,바위,보!\n당신: ");
+				me = scan.nextLine();
+			
+				if(me.equals("가위")) num=0;
+				else if (me.equals("바위")) num=1;
+				else num=2;
+			    
+				com = (int)(Math.random()*3);
+				if(com==0) computer="가위";
+				else if(com==1) computer="바위";
+				else computer="보";
+				
+				System.out.println("컴터: "+computer);
+				
+				if(num==com){
+					System.out.println("비겼습니다.");
+					break;
+				}
+				if((num==0 && com== 1)||num==1 && com== 2||num==2 && com== 0){
+					System.out.println("컴퓨터한테 졌습니다.");
+					comcount+=1;
+				}
+				if((num==1 && com== 0)||num==2 && com== 1||num==0 && com== 2){
+					System.out.println("당신이 이겼습니다.");
+					mecount+=1;
+				}
+				System.out.println("내가 이긴횟수: "+mecount+"   컴퓨터가 이긴횟수: "+comcount);
+				
+				System.out.println("한판더?? (y,n)");
+				re = scan.nextLine();
+				 if(re.equals("n")) {
+					break;
+				}
+			}
+		}
+	}
+}
