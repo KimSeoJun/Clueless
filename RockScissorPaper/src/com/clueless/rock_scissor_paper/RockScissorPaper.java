92682bce13b7970e1e07fab616a5fa636df56da5
김서준 <jskim332@naver.com>
2017-01-19 17:52:00 +0900

    수정본

--- RockScissorPaper/src/com/clueless/rock_scissor_paper/RockScissorPaper.java
+++ RockScissorPaper/src/com/clueless/rock_scissor_paper/RockScissorPaper.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class RockScissorPaper {
 	public static void main(String[] args) {
-		
+
 		String me;
 		int num;
 		String computer;
@@ -12,46 +12,51 @@ public class RockScissorPaper {
 		String re;
 		int mecount=0;
 		int comcount=0;
-		
+		boolean chcek = true;
 		Scanner scan = new Scanner(System.in);
 		
-		while(true){
-			System.out.println("");
-			while(true){
-				System.out.println("가위,바위,보!\n당신: ");
-				me = scan.nextLine();
+			while(chcek){
+				System.out.println("");
+				while(true){
+			System.out.println("가위,바위,보!\n당신: ");
+			me = scan.nextLine();
+		
+		
+			if(me.equals("가위")) num=0;
+			else if (me.equals("바위")) num=1;
+			else num=2;
+		    
+			com = (int)(Math.random()*3);
+			if(com==0) computer="가위";
+			else if(com==1) computer="바위";
+			else computer="보";
 			
-				if(me.equals("가위")) num=0;
-				else if (me.equals("바위")) num=1;
-				else num=2;
-			    
-				com = (int)(Math.random()*3);
-				if(com==0) computer="가위";
-				else if(com==1) computer="바위";
-				else computer="보";
-				
-				System.out.println("컴터: "+computer);
-				
-				if(num==com){
-					System.out.println("비겼습니다.");
-					break;
-				}
-				if((num==0 && com== 1)||num==1 && com== 2||num==2 && com== 0){
-					System.out.println("컴퓨터한테 졌습니다.");
-					comcount+=1;
-				}
-				if((num==1 && com== 0)||num==2 && com== 1||num==0 && com== 2){
-					System.out.println("당신이 이겼습니다.");
-					mecount+=1;
-				}
-				System.out.println("내가 이긴횟수: "+mecount+"   컴퓨터가 이긴횟수: "+comcount);
-				
-				System.out.println("한판더?? (y,n)");
-				re = scan.nextLine();
-				 if(re.equals("n")) {
-					break;
-				}
+			System.out.println("컴터: "+computer);
+			
+			
+			if((num==0 && com== 1)||num==1 && com== 2||num==2 && com== 0){
+				System.out.println("컴퓨터한테 졌습니다.");
+				comcount+=1;
 			}
-		}
+			else if((num==1 && com== 0)||num==2 && com== 1||num==0 && com== 2){
+				System.out.println("당신이 이겼습니다.");
+				mecount+=1;
+			}
+			else{
+				System.out.println("비겼습니다.");
+				break;
+			}
+			
+			System.out.println("내가 이긴횟수: "+mecount+"   컴퓨터가 이긴횟수: "+comcount);
+			
+			System.out.println("한판더?? (y,n)");
+			re = scan.nextLine();
+			 if(re.equals("n")) {
+				 chcek=false;
+				 break;
+			}
+		 }
+       }
 	}
-}
+}	
+	
